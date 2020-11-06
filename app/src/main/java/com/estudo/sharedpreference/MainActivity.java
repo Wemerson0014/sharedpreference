e6e2f9900232b314ef4c2abd365a4669e63de848
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-11-06 13:57:19 -0300

    Base do projeto

---
+++ app/src/main/java/com/estudo/sharedpreference/MainActivity.java
@@ -0,0 +1,15 @@
+package com.estudo.sharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
